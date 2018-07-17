779bfd6964dd6ca2e83f93ae5708683e0ab735e6
Hagan <haganpratt@gmail.com>
2018-07-17 11:51:19 -0400

    ...

--- DS_Take_2/src/main/java/com/HP/Dojo_Overflow/DsTake2Application.java
+++ DS_Take_2/src/main/java/com/HP/Dojo_Overflow/DsTake2Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DojoScriptTake2Application {
+public class DsTake2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DojoScriptTake2Application.class, args);
+		SpringApplication.run(DsTake2Application.class, args);
 	}
 }
--- DS_Take_2/src/test/java/com/HP/Dojo_Overflow/DsTake2ApplicationTests.java
+++ DS_Take_2/src/test/java/com/HP/Dojo_Overflow/DsTake2ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DojoScriptTake2ApplicationTests {
+public class DsTake2ApplicationTests {
 
 	@Test
 	public void contextLoads() {
